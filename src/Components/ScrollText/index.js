98b6f849478cc766a531c620599008afb19d0db5
Michael J. Lyons <mikelyons7@me.com>
2018-08-20 14:08:36 -0600

    Build out ScrollText component shell

---
+++ src/Components/ScrollText/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ScrollText.css";
+
+const ScrollText = () => {
+  return (
+    <div>
+      <h1>ScrollText</h1>
+    </div>
+  );
+};
+
+export default ScrollText;

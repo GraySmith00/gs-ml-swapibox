b3633f03e9b477445e93b74bfe4ca1a732285a0b
Michael J. Lyons <mikelyons7@me.com>
2018-08-20 16:56:54 -0600

    Update scrolltext to hide once completed and to stay within container

--- src/Components/ScrollText/ScrollText.test.js
+++ src/Components/ScrollText/ScrollText.test.js
@@ -1,1 +1,15 @@
 import React from "react";
+import ScrollText from "./index";
+import { shallow } from "enzyme";
+
+describe("ScrollText Component", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<ScrollText />);
+  });
+
+  it.skip("should update state when component is mounted", () => {
+    expect(wrapper.state().currentQuote).not.toEqual("");
+  });
+});

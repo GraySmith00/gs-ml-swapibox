4257f055425881297f6e7de2000132dd004a10e4
Michael J. Lyons <mikelyons7@me.com>
2018-08-20 16:37:45 -0600

    Update helpers to include title and film date

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,10 @@
 export const randomOpeningQuote = filmData => {
   const openingCrawls = filmData.results.map(film => {
-    return film.opening_crawl;
+    return {
+      title: film.title,
+      date: film.release_date,
+      quote: film.opening_crawl
+    };
   });
   const index = Math.floor(Math.random() * openingCrawls.length) + 1;
   return openingCrawls[index];

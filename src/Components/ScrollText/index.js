2452ded42ec861b1974ebc30e3249d1a27d54236
Michael J. Lyons <mikelyons7@me.com>
2018-08-20 16:39:54 -0600

    Update state in scrolltext component to include title and date

--- src/Components/ScrollText/index.js
+++ src/Components/ScrollText/index.js
@@ -6,7 +6,9 @@ class ScrollText extends Component {
   constructor() {
     super();
     this.state = {
-      currentQuote: ""
+      title: "",
+      date: "",
+      quote: ""
     };
   }
 
@@ -15,17 +17,24 @@ class ScrollText extends Component {
     fetch(url)
       .then(res => res.json())
       .then(data => {
-        const currentQuote = randomOpeningQuote(data);
-        this.setState({ currentQuote });
+        const { title, date, quote } = randomOpeningQuote(data);
+        this.setState({ title, date, quote });
       })
       .catch(error => console.log(error));
   }
 
   render() {
+    const { title, date, quote } = this.state;
     return (
-      <div>
-        <p>{this.state.currentQuote}</p>
-      </div>
+      <section className="scrolltext-container">
+        <div className="crawl">
+          <p>{quote}</p>
+          <div className="film-info">
+            <p>{title}</p>
+            <h1>{date}</h1>
+          </div>
+        </div>
+      </section>
     );
   }
 }

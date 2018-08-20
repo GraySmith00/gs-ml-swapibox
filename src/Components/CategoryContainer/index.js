c94b196f52c79c5ec11b295b4b5fdaa75779268a
Gray Smith <gray.smith00@gmail.com>
2018-08-20 17:28:05 -0600

    Build out CategoryContainer component

---
+++ src/Components/CategoryContainer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CategoryContainer.css';
+
+const CategoryContainer = () => {
+  return (
+    <div className="category-container">
+      <h1>CategoryContainer</h1>
+    </div>
+  );
+};
+
+export default CategoryContainer;

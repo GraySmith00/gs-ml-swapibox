10644cf52f3b07dee4cb124ec48bf24d77001ffc
Gray Smith <gray.smith00@gmail.com>
2018-08-20 17:29:25 -0600

    Add nav button to App render

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,6 +1,9 @@
-import React, { Component } from "react";
-import "./App.css";
-import ScrollText from "../ScrollText";
+import React, { Component } from 'react';
+
+import './App.css';
+
+import ScrollText from '../ScrollText';
+import CategoryContainer from '../CategoryContainer';
 
 class App extends Component {
   render() {
@@ -9,6 +12,12 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">SWAPIbox</h1>
         </header>
+        <nav className="nav-btns">
+          <button className="people-btn">People</button>
+          <button className="planets-btn">Planets</button>
+          <button className="vehicles-btn">Vehicles</button>
+        </nav>
+        <CategoryContainer />
         <ScrollText />
       </div>
     );

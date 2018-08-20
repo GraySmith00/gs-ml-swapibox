d6dad9c96395720f152b9a538372393a138fd446
Michael J. Lyons <mikelyons7@me.com>
2018-08-20 13:49:19 -0600

    Change file structure and add registerServiceWorker back in

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">SWAPI-box</h1>
         </header>
       </div>
     );

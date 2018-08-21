207dd8fe9cf5d0c81f0ad0173a5a22cb5c0da877
Michael J. Lyons <mikelyons7@me.com>
2018-08-20 19:34:03 -0600

    Add categoryContainer state

--- src/Components/CategoryContainer/index.js
+++ src/Components/CategoryContainer/index.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { fetchData, peopleList } from '../../helpers';
 
 class CategoryContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentData: []
+    };
+  }
   componentDidUpdate() {
     const { currentCategory } = this.props;
     const url = `https://swapi.co/api/${currentCategory}/`;
@@ -9,13 +15,23 @@ class CategoryContainer extends Component {
       fetchData(url)
         .then(res => res.json())
         .then(data => {
-          console.log(peopleList(data));
+          let currentData;
+          if (currentCategory === 'people') {
+            currentData = peopleList(data);
+            this.setState({
+              currentData
+            });
+          }
         })
         .catch(error => console.log(error));
     }
   }
 
   render() {
+    if (this.props.currentCategory) {
+      console.log(this.state.currentData);
+    }
+
     return (
       <div className="category-container">
         <h1>CategoryContainer</h1>

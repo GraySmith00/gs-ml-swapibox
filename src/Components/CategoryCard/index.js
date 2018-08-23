94dfa5f3e897b112ce2928112c3b940b2f31124a
Michael J. Lyons <mikelyons7@me.com>
2018-08-22 18:12:19 -0600

    Add favorite star and reposition

--- src/Components/CategoryCard/index.js
+++ src/Components/CategoryCard/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CategoryCard = ({ item }) => {
-  const cards = Object.keys(item).map(
-    (itemKey, index) =>
-      itemKey === 'name' ? (
-        <h3 key={`${itemKey}-${index}`}>{item[itemKey]}</h3>
-      ) : (
-        <p key={`${itemKey}-${index}`}>{`${itemKey}: ${item[itemKey]}`}</p>
-      )
-  );
+  const cards = Object.keys(item).map((itemKey, index) => {
+    switch (itemKey) {
+      case 'name':
+        return <h3 key={`${itemKey}-${index}`}>{item[itemKey]}</h3>;
+      case 'favorite':
+        return item.favorite ? (
+          <i className="fas fa-star" key={`${itemKey}-${index}`} />
+        ) : (
+          <i className="far fa-star" key={`${itemKey}-${index}`} />
+        );
+      default:
+        return (
+          <p key={`${itemKey}-${index}`}>{`${itemKey}: ${item[itemKey]}`}</p>
+        );
+    }
+  });
   return <div>{cards}</div>;
 };
 

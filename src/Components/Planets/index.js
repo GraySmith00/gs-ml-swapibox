28986a9fcc47c7873e5922fd6472e80cec1d5cf7
Gray Smith <gray.smith00@gmail.com>
2018-08-23 14:50:25 -0600

    Build out Planets component

---
+++ src/Components/Planets/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Planets extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Planets</h1>
+      </div>
+    );
+  }
+}
+
+export default Planets;

2fa2f5af265db57913e86c53cf7770a635b14160
Michael J. Lyons <mikelyons7@me.com>
2018-08-23 15:06:22 -0600

    Add proptypes and build out planet component :art:

--- src/Components/Planets/index.js
+++ src/Components/Planets/index.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CategoryCard from '../CategoryCard';
+
+import { initialFetchCall } from '../../helpers';
 
 class Planets extends Component {
+  constructor() {
+    super();
+    this.state = {
+      planetData: []
+    };
+  }
+
+  async componentDidMount() {
+    const planetData = await initialFetchCall('planets');
+    this.setState({ planetData });
+  }
+
   render() {
+    const { planetData } = this.state;
+    const displayPlanets = planetData.map((planet, index) => (
+      <CategoryCard
+        key={`${planet.name}=${index}`}
+        item={planet}
+        toggleFavorite={this.props.toggleFavorite}
+      />
+    ));
     return (
       <div>
         <h1>Planets</h1>
+        {displayPlanets}
       </div>
     );
   }
 }
+
+Planets.propTypes = {
+  toggleFavorite: PropTypes.func.isRequired
+};
 
 export default Planets;

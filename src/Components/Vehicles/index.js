9da2d167559785a125e4043f6c120ad3e21f1a55
Gray Smith <gray.smith00@gmail.com>
2018-08-23 14:50:51 -0600

    Build out Vehicles component

---
+++ src/Components/Vehicles/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Vehicles extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Vehicles</h1>
+      </div>
+    );
+  }
+}
+
+export default Vehicles;

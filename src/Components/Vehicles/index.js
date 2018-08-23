c4ecc5759da324451d99a33b9ac91c2f8d3abebc
Gray Smith <gray.smith00@gmail.com>
2018-08-23 15:06:28 -0600

    Finish building Vehicles component

--- src/Components/Vehicles/index.js
+++ src/Components/Vehicles/index.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CategoryCard from '../CategoryCard';
+import { initialFetchCall } from '../../helpers';
 
 class Vehicles extends Component {
+  constructor() {
+    super();
+    this.state = {
+      vehicleData: []
+    };
+  }
+
+  async componentDidMount() {
+    const vehicleData = await initialFetchCall('vehicle');
+    this.setState({ vehicleData });
+  }
+
   render() {
+    const { vehicleData } = this.state;
+    const displayVehicles = vehicleData.map((vehicle, index) => (
+      <CategoryCard
+        key={`${vehicle.name}-${index}`}
+        item={vehicle}
+        toggleFavorite={this.props.toggleFavorite}
+      />
+    ));
+
     return (
       <div>
         <h1>Vehicles</h1>
+        {displayVehicles}
       </div>
     );
   }
 }
+
+Vehicles.propTypes = {
+  toggleFavorite: PropTypes.func.isRequired
+};
 
 export default Vehicles;

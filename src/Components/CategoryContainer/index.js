c6739852d86633c86e6dab928ecea06d62983d9f
Michael J. Lyons <mikelyons7@me.com>
2018-08-26 15:47:38 -0600

    Add favoritedName to cards

--- src/Components/CategoryContainer/index.js
+++ src/Components/CategoryContainer/index.js
@@ -4,21 +4,30 @@ import PropTypes from 'prop-types';
 import CategoryCard from '../CategoryCard';
 import './CategoryContainer.css';
 
-const CategoryContainer = ({ currentData, toggleFavorite }) => {
-  let cards = currentData.map((item, index) => (
-    <CategoryCard
-      key={`${item}-${index}`}
-      item={item}
-      toggleFavorite={toggleFavorite}
-    />
-  ));
+const CategoryContainer = ({ currentData, toggleFavorite, favoritesNames }) => {
+  let cards = currentData.map((item, index) => {
+    const foundItem = favoritesNames.find(name => name === item.name);
+    if (foundItem) {
+      item.favorite = true;
+    } else {
+      item.favorite = false;
+    }
+    return (
+      <CategoryCard
+        key={`${item}-${index}`}
+        item={item}
+        toggleFavorite={toggleFavorite}
+      />
+    );
+  });
 
   return <div className="category-container">{cards}</div>;
 };
 
 CategoryContainer.propTypes = {
   currentData: PropTypes.array.isRequired,
-  toggleFavorite: PropTypes.func.isRequired
+  toggleFavorite: PropTypes.func.isRequired,
+  favoritesNames: PropTypes.array.isRequired
 };
 
 export default CategoryContainer;
